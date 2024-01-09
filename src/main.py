d663666d4b5dcf59bce3ec0bd39479f60f2fee92
Oleg Musatov <olegmusatov97@gmail.com>
2024-01-10 00:59:14 +0700

    Add and connect application auth

--- src/main.py
+++ src/main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
 
+from auth.schemas import UserRead, UserCreate
+from auth.base_config import auth_backend, fastapi_users
+
 app = FastAPI(
     title='Blitz Market'
 )
 
 
-@app.get('/')
-def hello():
-    return 'Hello world!'
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix='/auth/jwt',
+    tags=['auth']
+)
+
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
